4913ff86e0e6dd182fff1bec695e101240a05973
Protik Banerji <protik09@gmail.com>
2021-03-19 02:21:39 +0100

    Coremark port for single core testing

---
+++ src/Coremark-RP2040.h
@@ -0,0 +1,11 @@
+/**
+ * @file      Coremark-RP2040.h
+ * @authors   Protik Banerji <protik09@gmail.com>
+ * @copyright Protik Banerji, 2021
+ *
+ * @brief Coremark port for the RP2040
+ */
+/*******************************************************************************
+* Includes
+*******************************************************************************/
+
