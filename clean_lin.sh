bf23fb8a3ef793f6adbf8ad6dcdccf1e1085c04f
Protik Banerji <protik09@users.noreply.github.com>
2024-04-05 16:35:04 +0200

    Add directory cleaning scripts

---
+++ clean_lin.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Directories to be removed
+dirs_to_remove=("CMakeFiles" "CMakeScripts" "build" "build" "generated" "pico-sdk")
+
+# Files to be removed
+files_to_remove=("cmake_install.cmake" "CMakeCache.txt" "Makefile" "CMakeLists.txt.user" "CMakeDoxygenDefaults.cmake" "CMakeDoxyfile.in" "*.map")
+
+# Function to remove directories and files
+function cleanup {
+    for dir in "${dirs_to_remove[@]}"; do
+        find . -type d -name "$dir" -exec rm -rf {} +
+    done
+
+    for file in "${files_to_remove[@]}"; do
+        find . -type f -name "$file" -exec rm -f {} +
+    done
+}
+
+# Call the cleanup function
+make clean || cleanup
---
+++ clean_win.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal
+
+:: Directories to be removed
+set "dirs_to_remove=CMakeFiles CMakeScripts build generated pico-sdk"
+
+:: Files to be removed
+set "files_to_remove=cmake_install.cmake CMakeCache.txt Makefile CMakeLists.txt.user CMakeDoxygenDefaults.cmake CMakeDoxyfile.in *.ninja* *.map"
+
+:: Function to remove directories and files
+for %%d in (%dirs_to_remove%) do (
+    for /r . %%D in (%%d) do (
+        if exist "%%D" (
+            echo Removing directory "%%D"
+            rd /s /q "%%D"
+        )
+    )
+)
+
+for %%f in (%files_to_remove%) do (
+    for /r . %%F in (%%f) do (
+        if exist "%%F" (
+            echo Removing file "%%F"
+            del /f /q "%%F"
+        )
+    )
+)
+
+endlocal
